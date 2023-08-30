cc3941b43794fb02d3616d308f7aff71f6248c0f
Jean A. Flaherty <s1292010@u-aizu.ac.jp>
2023-08-30 12:19:34 +0900

    added pattern_plane1

---
+++ run_exps.bash
@@ -0,0 +1,18 @@
+DATASET_DIR=~/Datasets/NeRF/nerf-geometry-analysis
+FAST_METHODS=("instant-ngp" "tensorf")
+SLOW_METHODS=("vanilla-nerf" "neus" "mipnerf")
+# DATASETS=($(ls -d "$DATASET_DIR"/*))
+DATASETS=("pattern_plane1" "checkered_plane" "black_line_bg_white" "black_square_bg_white")
+DATASETS=("$DATASET_DIR/${DATASETS[@]}")
+
+for METHOD in "${FAST_METHODS[@]}"; do
+  for DATASET in "${DATASETS[@]}"; do
+    echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data"
+    ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data
+  done
+done
+
+for METHOD in "${SLOW_METHODS[@]}"; do
+  echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data ~/Datasets/NeRF/nerf-geometry-analysis/pattern_plane1 nerfstudio-data"
+  ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data ~/Datasets/NeRF/nerf-geometry-analysis/pattern_plane1 nerfstudio-data
+done
