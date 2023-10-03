fb8ec169894474045478cfb9d648b649268a7e40
Jean A. Flaherty <s1292010@u-aizu.ac.jp>
2023-10-03 14:00:22 +0900

    update

--- run_exps.bash
+++ run_exps.bash
@@ -4,7 +4,7 @@ DATASET="$1"
 # SLOW_METHODS=("vanilla-nerf" "neus" "mipnerf")
 # METHODS=("instant-ngp" "kplanes" "instant-ngp-bounded" "tensorf" "nerfacto")
 # METHODS=("tensorf" "instant-ngp" "kplanes" "instant-ngp-bounded" "nerfacto")
-METHODS=("tensorf" "instant-ngp" "nerfacto")
+METHODS=("instant-ngp" "tensorf" "nerfacto")
 # METHODS=("tensorf" "instant-ngp" "kplanes" "nerfacto")
 # DATASETS=($(ls -d "$DATASET_DIR"/*))
 DATASETS=("pattern_plane1" "checkered_plane" "black_line_bg_white" "black_square_bg_white")
@@ -12,15 +12,15 @@ DATASETS=("$DATASET_DIR/${DATASETS[@]}")
 
 # for METHOD in "${FAST_METHODS[@]}"; do
 #   for DATASET in "${DATASETS[@]}"; do
-#     echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data --data $DATASET"
-#     ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data --data $DATASET
+#     echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nga-data --data $DATASET"
+#     ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nga-data --data $DATASET
 #   done
 # done
 
 # for METHOD in "${SLOW_METHODS[@]}"; do
 #   DATASET=~/Datasets/NeRF/nerf-geometry-analysis/pattern_plane1_720x480
-#   echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data --data $DATASET"
-#   ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nerfstudio-data --data $DATASET
+#   echo "ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nga-data --data $DATASET"
+#   ns-train $METHOD --vis "viewer" --viewer.quit-on-train-completion True --data $DATASET nga-data --data $DATASET
 # done
 
 for METHOD in "${METHODS[@]}"; do
