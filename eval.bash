b01fee09ef57856ef412c3b28c51a0df017977da
kobejean <s1292010@u-aizu.ac.jp>
2023-10-25 14:24:31 +0900

    update

--- eval.bash
+++ eval.bash
@@ -2,6 +2,7 @@
 
 DATA_PATH="$1"
 
+rm -rf "${DATA_PATH}/renders"
 ns-eval --load-config "${DATA_PATH}/config.yml" \
         --render-output-path "${DATA_PATH}/renders" \
         --output-path "${DATA_PATH}/results.json"
